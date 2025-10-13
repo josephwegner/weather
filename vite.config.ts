0daec5c739f4373edb5b88a3eab7ed02b8702955
Joe Wegner <joe@wegnerdesign.com>
2025-10-13 12:27:10 -0500

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,27 @@ export default defineConfig({
         background_color: '#0f172a',
         display: 'standalone',
         scope: '/',
-        start_url: '/'
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
       }
     })
   ],
